8920e222d65808f248ea4f1cfcb649a2aebcbac9
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-22 17:24:06 -0300

    Documentación

--- Liskov Substitution/Program.cs
+++ Liskov Substitution/Program.cs
@@ -10,6 +10,26 @@ namespace Liskov_Substitution
     {
         static void Main(string[] args)
         {
+            /* --------------------Teoría--------------------*/
+            /*  
+                Este principio se vasa en tratar de mantener la herarquia de las 
+                clases de tal manera que el sentido de elllas sea correcto.
+                
+                EXTENSIÓN del principio OPEN/CLOSED.
+                
+                Una CLASE DERIVADA puede ser REEMPLAZADA por cualquier OTRA QUE USE
+                SU CLASE BASE sin ALTERAR el correcto FUNCIONAMIENTO de un programa.
+
+                Si una función espera como PARÁMETRO UNA CLASE BASE esta puede ser 
+                REEMPLAZADA POR CUALQUIER clase derivada.
+                
+                Una SUBCLASE NO DEBE REMOVER NI MODICAR COMPORTAMIENTO de la clase
+                base, NO DEBE CONOCER a los demas SUBTIPOS. 
+                
+                Si un SUBTIPO HACE ALGO QUE EL CLIENTE DEL SUPERTIPO NO ESPERA, es
+                una VIOLACIÓN al principio.
+            */
+            /* --------------------Teoría--------------------*/
         }
     }
 }
