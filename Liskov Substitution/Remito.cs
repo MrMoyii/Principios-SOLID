92e143afb36689c6ff9a4b3c47d5a022313c385c
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-23 18:11:18 -0300

    sacamos a remito de la herarquia de Documento

--- Liskov Substitution/Remito.cs
+++ Liskov Substitution/Remito.cs
@@ -6,18 +6,23 @@ using System.Threading.Tasks;
 
 namespace Liskov_Substitution
 {
-    internal class Remito : Documento
+    internal class Remito //: Documento
     {
-        public Remito(int numero, DateTime fecha, int bultos) : base(numero, fecha)
+        public Remito(int numero, DateTime fecha, int bultos) //: base (numero, fecha)
         {
             CantBultos = bultos;
+            Numero = numero;
+            Fecha = fecha;
         }
 
         public int CantBultos { get; set; }
+        public int Numero { get; set; }
+        public DateTime Fecha { get; set; }
+
 
         /*Aca es donde se viola el principio de sustitucion de Liskov porque esta
           modificando el comportamiento de una funcion de la clase padre.*/
-        public override string Descripcion()
+        public string Descripcion()
         {
             return $"Remito nro {Numero} de fecha {Fecha.ToShortDateString()} con {CantBultos} bultos";
         }
