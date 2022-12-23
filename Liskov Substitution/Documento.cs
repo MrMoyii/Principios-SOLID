cce026594f7f8e00e8827b9c6d7dd4e09aed6606
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-23 17:50:21 -0300

    Clase base Documento

---
+++ Liskov Substitution/Documento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution
+{
+    internal class Documento
+    {
+        protected string _sigla;
+        public Documento(int numero, DateTime fecha)
+        {
+            Numero = numero;
+            Fecha = fecha;
+        }
+        public DateTime Fecha { get; set; }
+        public int Numero { get; set; }
+        public string Imprimir()
+        {
+            return $"Imprimiendo {Descripcion()}";
+        }
+        public virtual string Descripcion()
+        {
+            return $"{_sigla} nro {Numero} de fecha {Fecha.ToShortDateString()}";
+        }
+    }
+}
