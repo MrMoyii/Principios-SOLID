766956b56328668454e86301aeafb27a11c1172d
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-23 17:59:17 -0300

    Clase Remito y documentacion de la violacion al principio

---
+++ Liskov Substitution/Remito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution
+{
+    internal class Remito : Documento
+    {
+        public Remito(int numero, DateTime fecha, int bultos) : base(numero, fecha)
+        {
+            CantBultos = bultos;
+        }
+
+        public int CantBultos { get; set; }
+
+        /*Aca es donde se viola el principio de sustitucion de Liskov porque esta
+          modificando el comportamiento de una funcion de la clase padre.*/
+        public override string Descripcion()
+        {
+            return $"Remito nro {Numero} de fecha {Fecha.ToShortDateString()} con {CantBultos};
+        }
+    }
+}
