7f063881b61f4cecb81f530b17da121e7ceb8f0c
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-23 17:54:10 -0300

    Clases Factura y NotaCrecito bases para la recatorización

---
+++ Liskov Substitution/NotaCredito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution
+{
+    internal class NotaCredito : Documento
+    {
+        public NotaCredito(int numero, DateTime fecha) : base(numero, fecha)
+        {
+            _sigla = "NC";
+        }
+    }
+}
