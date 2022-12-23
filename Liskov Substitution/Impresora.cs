8da02379caf3c58df129be367f361ee6037783fb
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-23 18:02:30 -0300

    Clase Impresora

---
+++ Liskov Substitution/Impresora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution
+{
+    internal class Impresora
+    {
+        public void ImprimirRemito(Remito unRemito)
+        {
+            Console.WriteLine(unRemito.Imprimir());
+        }
+        public void ImprimirFactura(Remito unaFactura)
+        {
+            Console.WriteLine(unaFactura.Imprimir());
+        }
+        public void ImprimirNotaCredito(Remito unaNotaCredito)
+        {
+            Console.WriteLine(unaNotaCredito.Imprimir());
+        }
+    }
+}
