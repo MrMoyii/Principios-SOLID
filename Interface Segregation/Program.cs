3ed7701d22fc92b734fefe601856a7b833264338
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-24 14:32:57 -0300

    Creacion de clase y documentacion de este.

---
+++ Interface Segregation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* --------------------Teoría--------------------*/
+            /*  
+                Los CLIENTES no deben estar FORZADOS a IMPLEMENTAR INTERFACES
+                QUE NO USAN.
+
+                Guarda relación cno la COHESIÓN de las aplicacionoes.
+
+                Las clases que implementen una interface oo una clase abstracta
+                NO DEBERÍAN ESTAR OBLIGADOS A UTILIZAR PARTES QUE NO VAN A UTILIZAR.
+
+                Los CLIENTES NO DEBEN ESTAR OBLIGADS A IMPLEMENTAR y/ a depender
+                de una INTERFACE QUE LUEGO NO USARÁN.
+            */
+            /* --------------------Teoría--------------------*/
+        }
+    }
+}
