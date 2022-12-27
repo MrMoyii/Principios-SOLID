fea21f7d2caac806132ed375aca97f0397d011e6
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-27 15:37:04 -0300

    Clase Documento

---
+++ Interface Segregation/Documento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation
+{
+    internal abstract class Documento
+    {
+        public Documento(DateTime fecha, int numero)
+        {
+            Fecha = fecha;
+            Numero = numero;
+        }
+
+        public DateTime Fecha { get; set; }
+        public int Numero { get; set; }
+        public abstract void Imprimir();
+        public abstract void EnviarPorEmail();
+    }
+}
