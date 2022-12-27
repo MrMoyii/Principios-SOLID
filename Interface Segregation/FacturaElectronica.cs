eeca57e39b93b38b1f893bbb9d05e084ff21b486
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-27 16:57:11 -0300

    clase FacturaElectronica

---
+++ Interface Segregation/FacturaElectronica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation
+{
+    internal class FacturaElectronica : Documento
+    {
+        public FacturaElectronica(int numero, DateTime fecha) : base(numero, fecha) { }
+        
+        public string CAE { get; set; }
+        public override void EnviarPorEmail()
+        {
+            Console.WriteLine($"Enviando por email la factura {Numero} del dia {Fecha.ToShortDateString()}");
+
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Imprimiend por email la factura {Numero} del dia {Fecha.ToShortDateString()}");
+
+        }
+    }
+}
