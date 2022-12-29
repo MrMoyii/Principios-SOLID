228e6cbcbb3e11573d3862d9d5585712c90156d2
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-28 23:57:24 -0300

    Clase Municipal

---
+++ Dependency Inversion/Municipal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class Municipal : Impuesto
+    {
+        public Municipal(double importe, string partida) : base(importe)
+        {
+            Partida = partida;
+        }
+        public string Partida { get; set; }
+    }
+}
