d9c808669648ead3fc3ab06117973ce3ee984db7
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-28 23:52:33 -0300

    Clase NotaCredito

---
+++ Dependency Inversion/NotaCredito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class NotaCredito : DocumentoContable
+    {
+        public NotaCredito(int numero, DateTime Fecha, double imprte) : base(numero, Fecha, imprte)
+        {
+            _sigla = "NC";
+        }
+
+        public override double Total()
+        {
+            return Importe * 1.21 * -1;//Ejemplo
+        }
+    }
+}
