25c8f1b6c72099b6ffa0fc3cc99391458767c718
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-28 23:58:43 -0300

    Clase FacturaLuz

---
+++ Dependency Inversion/FacturaLuz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class FacturaLuz : Impuesto
+    {
+        public FacturaLuz(double importe, string coodigoPago) : base(importe)
+        {
+            CoodigoPago = coodigoPago;
+        }
+        public string CoodigoPago { get; set; }
+    }
+}
