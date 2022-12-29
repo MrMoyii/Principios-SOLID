c6268c274bb41926dff5d7d34d02c9f31619506f
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-28 23:55:12 -0300

    Clase Importe

---
+++ Dependency Inversion/Impuesto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class Impuesto
+    {
+        public Impuesto(double importe)
+        {
+            Importe = importe;
+        }
+        public double Importe { get; set; }
+    }
+}
