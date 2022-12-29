96bd3e2b13e1fe0474055a04f15dde8eac014918
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-28 23:48:54 -0300

    Clase Documento Contable

---
+++ Dependency Inversion/DocumentoContable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal abstract class DocumentoContable
+    {
+        public DocumentoContable(int numero, DateTime fecha, double importe)
+        {
+            Numero = numero;
+            Fecha = fecha;
+            Importe = importe;
+        }
+
+        public DateTime Fecha { get; set; }
+        public double Importe { get; set; }
+        public int Numero { get; set; }
+
+        public abstract double Total();
+    }
+}
--- Dependency Inversion/Program.cs
+++ Dependency Inversion/Program.cs
@@ -10,6 +10,24 @@ namespace Dependency_Inversion
     {
         static void Main(string[] args)
         {
+            /* --------------------Teoría--------------------*/
+            /*  
+                Se utiliza para DESACOPLAR MÓDULOS de software.
+                
+                Las CLASES DE ALTO NIVEL no deberían depender de las clases
+                de bajo nivel. Ambas deberían DEPENDER DE LAS ABSTRACCIONES.
+
+                Las ABSTRACCIONES NO DEBERÍAN DEPENDER DE LOS DETALLES. Los
+                detalles deberían depender de las abtracciones.
+
+                Al diseñar la interacción entre un MÓDUL DE ALT NIVEL Y 
+                UNO DE BAJO NIVEL, la interacción debe considerarse UNA 
+                INTERACCIÓN ABTRACTA entre ellos.
+
+                "Lo que hace es revertir las dependencias para lograr
+                un mejor acoplamiento y una mejor cohesion."
+            */
+            /* --------------------Teoría--------------------*/
         }
     }
 }
