11f5450bd3713a06ddf1c3a6c07e73bba5523fc8
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-29 00:04:05 -0300

    Clase Remito

---
+++ Dependency Inversion/Remito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class Remito
+    {
+        public Remito(int numero, DateTime fecha, int cantBultos)
+        {
+            Numero= numero;
+            Fecha= fecha;
+            CantBultos= cantBultos;
+        }
+
+        public int Numero { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CantBultos { get; set; }
+    }
+}
