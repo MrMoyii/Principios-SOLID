f4901c941d6205d447ef1ce54bd03da9de87c9d4
Omar Moyano <omarmoyano7w@gmail.com>
2022-12-29 00:01:43 -0300

    Clase ReciboSueldo

---
+++ Dependency Inversion/ReciboSueldo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    internal class ReciboSueldo
+    {
+        public ReciboSueldo(int legago, double total)
+        {
+            Legago = legago;
+            Total = total;
+        }
+
+        public double Total { get; set; }
+        public int Legago { get; set; }
+    }
+    
+}
